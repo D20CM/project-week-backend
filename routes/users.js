ea154661b2542f87cbcbe304bf6a4aece9dcc44d
D20CM <scottbrown500@hotmail.com>
2022-01-18 14:23:32 +0000

    refresh

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { getAllUsers } from "../models/users.js";
+import { deleteUser, getAllUsers, updateUser } from "../models/users.js";
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -10,11 +10,10 @@ router.get("/", function (req, res, next) {
 export default router;
 
 router.get("/users", async function (req, res) {
-  if (req.query.name) {
-    console.log(req.query.name);
-    let searchedName = req.query.name;
-    // console.log("You are looking for " + searchedName);
-    const foundUser = await getUserByName(searchedName);
+  if (req.query.id) {
+    console.log(req.query.id);
+    let id = req.query.id;
+    const foundUser = await getUserById(id);
     res.json(foundUser);
   } else {
     const users = await getAllUsers();
@@ -22,8 +21,22 @@ router.get("/users", async function (req, res) {
   }
 });
 
-app.post("/users", async function (req, res) {
+router.post("/users", async function (req, res) {
   let newUser = req.body;
   const addedUser = await addUser(newUser);
   res.json(addedUser);
+});
+
+router.put("/users/:id", async function (req, res) {
+  //id???
+  let id = req.params.id; ///Number???-------------------------------
+  let newData = req.body;
+  const updatedUser = await updateUser(id, newData);
+  res.json(updatedUser);
+});
+
+router.delete("/users/:id", async function (req, res) {
+  let id = req.params.id; ///Number???---------------------------------
+  const deletedUser = await deleteUser(id);
+  res.json(deletedUser);
 });
