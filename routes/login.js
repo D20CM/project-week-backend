5f975e9ce49002e61e255987ece22aa31fb22eb2
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-01-19 18:38:05 +0000

    fix login

--- routes/login.js
+++ routes/login.js
@@ -18,11 +18,4 @@ router.post("/", async (req, res) => {
    });
 });
 
-// router.get("/", async (req, res) => {
-//    const userObject = await admin.auth().verifyIdToken(req.body.uid);
-
-//    res.json({
-//       success: true,
-//    });
-// });
 export default router;
